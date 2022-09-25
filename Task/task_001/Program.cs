f4d8199ba9c8dde9388aae1e62378675a7f6c285
ilya <ilya.0709.abvc@mail.ru>
2022-09-25 20:48:01 +0300

    ready task001

---
+++ Task/task_001/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число.\n Чтобы выйти из програмы нажмите Enter.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
